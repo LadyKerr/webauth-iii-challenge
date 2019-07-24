dc2083e8cdc91dda421dca28555d44c2c75c3ab1
LadyKerr <kadiekerr@gmail.com>
2019-07-24 09:51:39 -0400

    react-app and updates to auth

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const Users = require("../users/user-model.js");
 
@@ -26,14 +27,32 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
         res.status(200).json({
-          message: `Nice to see you ${user.username}!`
+          message: `Nice to see you ${user.username}! Here is your token`,
+          token
         });
       } else {
-        res.status(401).json({ message: "You shall not pass!" });
+        res
+          .status(401)
+          .json({ message: "You shall not pass! Invalid credentials." });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, options); //need secrets.jwtSecrets
+}
